7f44c369c9ef848400e50f4a8d003cbd627f9906
quev-dev <quevdev@outlook.com>
2023-01-26 17:30:45 -0500

    Made the text bigger for mobile.

--- src/1-todo.md
+++ src/1-todo.md
@@ -4,12 +4,12 @@ Hi Lucky
 
 ---
 
-### Issue with Emails
-* Rewrite the JS and find a solution to sending emails through FireFox and other browsers.
-    * The culprit may be in the timing.
+### ~~Issue with Emails~~
+* ~~Rewrite the JS and find a solution to sending emails through FireFox and other browsers.~~
+    * ~~The culprit may be in the timing.~~
 
-### Animations
-* Change the timing on the banner header animations to be faster. They're too slow.
+### ~~Animations~~
+* ~~Change the timing on the banner header animations to be faster. They're too slow.~~
 
 ### Font Size
 * The font is too small. Go in and change the font sizes to be fairly bigger; adjusting the section sizes as needed.
--- src/components/containers/banner.jsx
+++ src/components/containers/banner.jsx
@@ -20,8 +20,8 @@ function Banner() {
       className="animate__animated animate__fadeInLeft">
         {/* <img id="portrait-banner" src={portraitBanner} alt=""/> */}
         <h1 className="banner-header">QuevDev</h1>
-        <h3 className="banner-subheader animate__animated animate__fadeInLeft animate__delay-1s">Front-End Developer</h3>
-        <p id = "banner-caption" className="animate__animated animate__fadeInLeft animate__delay-2s">
+        <h3 className="banner-subheader animate__animated animate__fadeInLeft">Front-End Developer</h3>
+        <p id = "banner-caption" className="animate__animated animate__fadeInLeft">
           Working to create tools with engaging UI and UX.
         </p>
       </div>
@@ -35,7 +35,7 @@ function Banner() {
       {/* Scroll Indicator */}
       <div className="sticky-footer">
         <img id="scroll-indicator" 
-        className="animate__animated animate__flipInX animate__delay-3s"
+        className="animate__animated animate__flipInX animate__delay-1s"
         src={iconCaretdown} alt=""/>
       </div>
 
